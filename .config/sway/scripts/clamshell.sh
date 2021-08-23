5cf0f6c5cf0cb7a749a15168f9f3acf3d6640839
regfe89 <marat.ismailov889@gmail.com>
2021-08-23 21:57:01 +0600

    Congigs changed

--- .config/sway/scripts/clamshell.sh
+++ .config/sway/scripts/clamshell.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 if cat /proc/acpi/button/lid/LID/state | grep -q open; then
     swaymsg output <eDP-1> enable
+    swaymsg output <HDMI-A-1> disable
 else
-    swaymsg output <eDP-1> disable
+    
+    swaymsg output <HDMI-A-1> pos 0 0; swaymsg output <eDP-1> disable
 fi
