0bfff402c4bfc1d2c3224cddbafc02415def8fa0
regfe89 <marat.ismailov889@gmail.com>
2022-01-04 16:23:34 +0600

    Update .bash_profile, .bashrc, and 3 more files...

--- .config/i3/run_mel.sh
+++ .config/i3/run_mel.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-kitty -- /bin/bash -ec 'cd $HOME/projects/dev_docker_mel; docker-compose up' &
+kitty -- /bin/bash -ec 'cd $HOME/projects/dev_docker_mel; docker-compose up -d; bash' &
 sleep 1;
 kitty -- /bin/bash -ec 'cd $HOME/projects/dev_docker_mel/MEL; git status; bash'
